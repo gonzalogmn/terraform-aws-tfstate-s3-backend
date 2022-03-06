505b72b78b9636a5f24d2af5c2e0a96ea02db8e3
Gonzalo <ggimeno@nextdigital.es>
2022-03-06 14:43:59 +0100

    fix: variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-var "s3_bucket_name" {
+variable "s3_bucket_name" {
   default = "terraform-state-bucket"
 }
 
-var "dynamodb_table_name" {
+variable "dynamodb_table_name" {
   default = "terraform-state-lock"
 }
