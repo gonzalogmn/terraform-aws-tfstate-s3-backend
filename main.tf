023c29ffac817eb2761500c57935eccab86f791e
Gonzalo <ggimeno@nextdigital.es>
2022-03-06 14:46:26 +0100

    fix: names

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "aws_kms_key" "terraform-bucket-key" {
+resource "aws_kms_key" "terraform_bucket_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
   enable_key_rotation     = true
 }
 
-resource "aws_kms_alias" "key-alias" {
-  name          = "alias/terraform-bucket-key"
-  target_key_id = aws_kms_key.terraform-bucket-key.key_id
+resource "aws_kms_alias" "key_alias" {
+  name          = "alias/terraform_bucket_key"
+  target_key_id = aws_kms_key.terraform_bucket_key.key_id
 }
 
 
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "terraform_state" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
+        kms_master_key_id = aws_kms_key.terraform_bucket_key.arn
         sse_algorithm     = "aws:kms"
       }
     }
@@ -29,7 +29,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.terraform-state.id
+  bucket = aws_s3_bucket.terraform_state.id
 
   block_public_acls       = true
   block_public_policy     = true
